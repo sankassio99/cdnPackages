0664d248f71055d76de4bb290224623ec405df14
Kassio San <kciosantana@gmail.com>
2023-08-11 14:57:39 -0300

    Get MainMenu from Stratws

--- index.js
+++ index.js
@@ -2,119 +2,12 @@ const template = document.createElement('template');
 const link = document.createElement('link');
 const linkHref = "http://vli.cacatua.com/SIMPLE/Content/sw-css/azul/siteware.css?version=2104.6";
 
-const mainMenu = `
-<div class="main-menu-wrap mini-popover">
-    <a class="mm-trigger" id="main-menu-trigger" data-mini-popover="true"
-        data-mini-popover-conteudo="mini-popover-main-menu">
-        <svg>
-            <use xlink:href="/SIMPLE/Content/svg/modulos.svg#painel"></use>
-        </svg>
-        <span class="versao">2104.6</span>
-    </a>
-    <div id="mini-popover-main-menu" class="mp-box">
-        <div class="mp-content">
-            <div class="main-menu">
-                <div class="mm-ctrl">
-                    <ul class="mm-list">
-                        <li class="mm-featured-item">
-                            <a id="js-main-menu-close" class="cursor-pointer">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a id="main-menu-painel" href="/SIMPLE/MeuPainel" class="js-main-menu-switch"
-                                data-id="mm-sub-painel">
-                                <svg>
-                                    <use xlink:href="/SIMPLE/Content/svg/modulos.svg#painel"></use>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a id="main-menu-performance" href="/SIMPLE/PerformanceCorporativa"
-                                class="js-main-menu-switch" data-id="mm-sub-results">
-                                <svg>
-                                    <use xlink:href="/SIMPLE/Content/svg/modulos.svg#performance"></use>
-                                </svg>
-                            </a>
-                        </li>
-                        <li class="mm-ctrl-active">
-                            <a id="main-menu-melhorias" href="/SIMPLE/OportunidadesDeMelhoria"
-                                class="js-main-menu-switch" data-id="mm-sub-actions">
-                                <svg>
-                                    <use xlink:href="/SIMPLE/Content/svg/modulos.svg#melhorias"></use>
-                                </svg>
-                            </a>
-                        </li>
-
-
-
-                        <li>
-                            <a id="main-menu-admin" href="/SIMPLE/Configuracoes" class="js-main-menu-switch"
-                                data-id="mm-sub-admin">
-                                <svg>
-                                    <use xlink:href="/SIMPLE/Content/svg/modulos.svg#configuracoes"></use>
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-                <div class="mm-body js-listagem-menus">
-                    <div class="js-sub-list-wrap mm-sub-list-wrap" data-id="mm-sub-painel">
-                        <div class="mm-title">Meu Painel</div>
-                        <ul class="mm-sub-list">
-                            <li><a id="main-menu-painel-agenda"
-                                    href="/SIMPLE/MeuPainel#agenda"><span>Agenda</span></a></li>
-                            <li><a id="main-menu-painel-radar"
-                                    href="/SIMPLE/MeuPainel#radar"><span>Radar</span></a></li>
-                            <li><a id="main-menu-painel-unidadegerencial"
-                                    href="/SIMPLE/MeuPainel#unidadegerencial"><span>Radar da UG</span></a>
-                            </li>
-                            <li><a id="main-menu-painel-desempenho"
-                                    href="/SIMPLE/MeuPainel#desempenho"><span>Desempenho</span></a></li>
-                            <li><a id="main-menu-painel-tarefas"
-                                    href="/SIMPLE/MeuPainel#tarefas"><span>Tarefas</span></a></li>
-                            <li><a id="main-menu-painel-resumo"
-                                    href="/SIMPLE/MeuPainel#resumo"><span>Resumo</span></a></li>
-                        </ul>
-                    </div>
-                    <div class="js-sub-list-wrap mm-sub-list-wrap" data-id="mm-sub-results">
-                        <div class="mm-title">Performance Corporativa</div>
-                        <ul class="mm-sub-list">
-                            <li><a id="main-menu-performance-planilha"
-                                    href="/SIMPLE/PerformanceCorporativa/PlanilhaDeIndicadores"><span>Planilha</span></a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="js-sub-list-wrap mm-sub-list-wrap mm-body-active" data-id="mm-sub-actions">
-                        <div class="mm-title">Oportunidades de Melhoria</div>
-                        <ul class="mm-sub-list">
-
-
-
-                            <li><a id="main-menu-melhorias-iniciativas"
-                                    href="/SIMPLE/Initiatives"><span>Iniciativas</span></a></li>
-                        </ul>
-                    </div>
-                    <div class="js-sub-list-wrap mm-sub-list-wrap" data-id="mm-sub-admin">
-                        <div class="mm-title">Configurações</div>
-                        <ul class="mm-sub-list">
-                            <li><a id="main-menu-admin-configuracoes"
-                                    href="/SIMPLE/Configuracoes"><span>Configurações</span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-`
 
 template.innerHTML = `
 <header class="navbar navbar-default navbar-fixed-top" style="background-color:#3777bc">
     <div class="main-navbar">
         <div class="mn-left">
-            ${ mainMenu }
+            <div id="mainMenuSpace"></div>
         </div>
     </div>
 
@@ -138,7 +31,29 @@ class StratwsHeader extends HTMLElement {
         shadowDoom.appendChild(template.content);
     }
 
-    connectedCallback() {
+    async connectedCallback() {
+        await this.getMainMenuHtml();
+    }
+
+    async getMainMenuHtml() {
+        const promise = new Promise((res, rej) => {
+          fetch('/SIMPLE/Initiative/GetMainMenu')
+            .then(req => req.text())
+            .then((data) => {
+                this.writeMainMenu(data);
+            })
+            .catch((error) => console.error(error));
+        })
+    }
+
+    writeMainMenu(mainMenuHtml){
+        const mainMenuSpace = this.shadowRoot.querySelector("#mainMenuSpace");
+        mainMenuSpace.innerHTML = mainMenuHtml
+
+        this.activeMainMenuBehaviors()
+    }
+
+    activeMainMenuBehaviors(){
         const iconButton = this.shadowRoot.querySelector("#main-menu-trigger");
 
         const mainMenuPopup = this.shadowRoot.querySelector("#mini-popover-main-menu");
@@ -156,7 +71,6 @@ class StratwsHeader extends HTMLElement {
                 this.shadowRoot.querySelector(`.js-sub-list-wrap[data-id="${itemHoveredId}"]`).classList.add("mm-body-active")
             });
         });
-
     }
 }
 
