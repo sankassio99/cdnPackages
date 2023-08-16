e6e013630b1e34f2f261447f7a7f0b992533b4ee
Kassio San <kciosantana@gmail.com>
2023-08-16 11:37:07 -0300

    addInitiativeForm

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.dev.js',
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'production',
